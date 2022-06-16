141e113fcb9085e09da3d1745217f0fc3411bdb1
Ilija <ilijaarsenijevic@gmail.com>
2022-06-16 02:09:29 +0200

    dodata mobile.de podrska
    i dalje ubagovano jer mobile.de ima captcha zahtev za svako pristupanje iz pretrage bilo kom oglasu
    
    promenjen pretrazivac sa phantomjs na firefox
    
    malko optimizovaniji kod

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from time import mktime
 import time
 import rpc
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import JavascriptException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import os
 
 client_id = '879374063779336202'
@@ -11,9 +15,15 @@ rpc_obj = rpc.DiscordIpcClient.for_platform(client_id)  # Send the client ID to 
 time.sleep(2)
 start_time = mktime(time.localtime())
 
+options = Options()
+options.headless = True
+
 global originalTitle
+global imeSajta
 
 originalTitle = ""
+imeSajta = ""
+logo = ""
 
 while True:
 
@@ -25,24 +35,54 @@ while True:
     shortUrl = shortUrlCache.read()
     title = titleCache.read()
 
-
-
     if shortUrl == "www.polovniautomobili.com":
-
-        driver = webdriver.PhantomJS()
-        driver.get(url)
-        script = 'return document.getElementsByClassName("priceClassified")[0].innerText'
-        cena = driver.execute_script(script)
-        driver.quit()
-
+        imeSajta = "Polovni Automobili"
+        script1 = 'return document.getElementsByClassName("priceClassified")[0].innerText'
+        try:
+            driver = webdriver.Firefox(options=options, executable_path=r'geckodriver.exe')
+            driver.get(url) 
+            cena = driver.execute_script(script1)
+            driver.quit()
+        except JavascriptException:
+                cena = "~cena ne radi~"
         if title != originalTitle:
             originalTitle = title
         tekst = title
         pare = cena
+        logo = "logo"
+        
+    elif shortUrl == "suchen.mobile.de":
+        imeSajta = "mobile.de"
+        script2 = 'return document.getElementsByClassName("h3")[1].textContent'
+        spliturl = url.split("/")
+        
+        if spliturl[3] == "fahrzeuge":
+            spliturl1 = url.split("&")
+            sredjenurl1 = spliturl1[0] 
+            driver = webdriver.Firefox(options=options, executable_path=r'geckodriver.exe')
+            try:
+                driver.get(sredjenurl1)
+                cena = driver.execute_script(script2)
+                driver.quit()
+            except JavascriptException:
+                cena = "~cena ne radi~"
+            
+        elif spliturl[3] == "auto-inserat":
+            driver = webdriver.Firefox(options=options, executable_path=r'geckodriver.exe')
+            driver.get(url)
+            cena = driver.execute_script(script2)
+            driver.quit()
+            
+        if title != originalTitle:
+            originalTitle = title
+        tekst = title
+        pare = cena
+        logo = "mobile"
+        
     else:
         tekst = ""
         pare = ""
-
+        
     activity = {
             "state": pare,  # anything you like
             "details": tekst,  # anything you like
@@ -50,12 +90,11 @@ while True:
                 "start": start_time
             },
             "assets": {
-                "small_text": "Polovni Automobili",  # anything you like
-                "small_image": "logo",  # must match the image key
-                "large_text": "Polovni Automobili",  # anything you like
-                "large_image": "logo"  # must match the image key
+                "small_text": imeSajta,  # anything you like
+                "small_image": logo,  # must match the image key
+                "large_text": imeSajta,  # anything you like
+                "large_image": logo  # must match the image key
             }
         }
     rpc_obj.set_activity(activity)
     time.sleep(10)
-
