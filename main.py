017a640e662eb4c86df13b36356fcdb66ba2684e
Ilija <ilijaarsenijevic@gmail.com>
2022-06-16 03:35:20 +0200

    brzi update nakon mobile.de integracije
    na brzaka

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import JavascriptException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import pickle
 import os
 
 client_id = '879374063779336202'
@@ -17,6 +18,11 @@ start_time = mktime(time.localtime())
 
 options = Options()
 options.headless = True
+user_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/19C63 [FBAN/FBIOS;FBDV/iPhone12,8;FBMD/iPhone;FBSN/iOS;FBSV/15.2.1;FBSS/2;FBID/phone;FBLC/en_US;FBOP/5]'
+profile = webdriver.FirefoxProfile()
+options.add_argument("--width=640")
+options.add_argument("--height=480")
+profile.set_preference('general.useragent.override', user_agent)
 
 global originalTitle
 global imeSajta
@@ -42,7 +48,7 @@ while True:
             driver = webdriver.Firefox(options=options, executable_path=r'geckodriver.exe')
             driver.get(url) 
             cena = driver.execute_script(script1)
-            driver.quit()
+            driver.close()
         except JavascriptException:
                 cena = "~cena ne radi~"
         if title != originalTitle:
@@ -51,27 +57,42 @@ while True:
         pare = cena
         logo = "logo"
         
-    elif shortUrl == "suchen.mobile.de":
+    elif shortUrl == "suchen.mobile.de" or shortUrl == "m.mobile.de":
         imeSajta = "mobile.de"
         script2 = 'return document.getElementsByClassName("h3")[1].textContent'
+        script3 = 'return document.getElementsByClassName("_1CIdp")[1].innerText'
         spliturl = url.split("/")
         
-        if spliturl[3] == "fahrzeuge":
-            spliturl1 = url.split("&")
-            sredjenurl1 = spliturl1[0] 
-            driver = webdriver.Firefox(options=options, executable_path=r'geckodriver.exe')
-            try:
-                driver.get(sredjenurl1)
+        try:
+            if spliturl[3] == "fahrzeuge":
+                spliturl1 = url.split("&")
+                sredjenurl1 = spliturl1[0]
+                print(sredjenurl1)
+                driver = webdriver.Firefox(options=options, executable_path=r'geckodriver.exe', firefox_profile=profile)
+                try:
+                    driver.get(sredjenurl1)
+                    #pickle.dump(driver.get_cookies(), open("cookies.pkl","wb"))
+                    #cookies = pickle.load(open("cookies.pkl", "rb"))
+                    #for cookie in cookies:
+                    #    driver.add_cookie(cookie)
+                    cena = driver.execute_script(script3)
+                    print(cena)
+                    driver.close()
+                except JavascriptException:
+                    cena = "~cena ne radi~"
+                
+            elif spliturl[3] == "auto-inserat":
+                driver = webdriver.Firefox(options=options, executable_path=r'geckodriver.exe')
+                driver.get(url)
                 cena = driver.execute_script(script2)
-                driver.quit()
-            except JavascriptException:
-                cena = "~cena ne radi~"
-            
-        elif spliturl[3] == "auto-inserat":
-            driver = webdriver.Firefox(options=options, executable_path=r'geckodriver.exe')
-            driver.get(url)
-            cena = driver.execute_script(script2)
-            driver.quit()
+                driver.close()
+            else:
+                title = "~u pretrazi~"
+                cena = "~trenutno bez cene~"
+                continue
+        except IndexError:
+            continue
+        
             
         if title != originalTitle:
             originalTitle = title
